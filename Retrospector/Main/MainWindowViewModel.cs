854755208553d7ed9e3fafccd0af8ac825ceb56d
NonlinearFruit <1123benji5813@gmail.com>
2021-06-18 10:30:12 -0500

    Remove dead code

--- Retrospector/Main/MainWindowViewModel.cs
+++ Retrospector/Main/MainWindowViewModel.cs
@@ -1,17 +1,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Retrospector.MediaTab.Interfaces;
-using Retrospector.Search.Interfaces;
 using Retrospector.SearchTab.Interfaces;
 
 namespace Retrospector.Main
 {
     public class MainWindowViewModel
     {
-        private readonly IQueryBuilder _queryBuilder;
-        private readonly ISearchDataGateway _searchDataGateway;
-
-        public ICollection<object> Tabs { get; set; }
+        public ICollection<object> Tabs { get; }
 
         public MainWindowViewModel(ISearchTab searchTab, IMediaTab mediaTab)
         {
